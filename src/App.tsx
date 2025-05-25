08750db62cc2e095072fe1714ae0e47924cb6822
Sreekanth <spayyavula@gmail.com>
2025-05-25 07:33:39 -0700

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,10 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import Layout from './components/common/Layout';
-import Dashboard from './pages/Dashboard';
-import OptionsChain from './pages/OptionsChain';
-import Portfolio from './pages/Portfolio';
-import Watchlist from './pages/Watchlist';
-import StrategyBuilder from './pages/StrategyBuilder';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Dashboard />} />
-          <Route path="/options-chain" element={<OptionsChain />} />
-          <Route path="/portfolio" element={<Portfolio />} />
-          <Route path="/watchlist" element={<Watchlist />} />
-          <Route path="/strategy-builder" element={<StrategyBuilder />} />
-          <Route path="*" element={<Navigate to="/" replace />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <p>Start prompting (or editing) to see magic happen :)</p>
+    </div>
   );
 }
 
