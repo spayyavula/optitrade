3f80535525057233f83737b4a980f27b0b83759e
Sreekanth <spayyavula@gmail.com>
2025-05-25 19:27:33 -0700

    Set up React Router and routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Layout from './components/common/Layout';
+import Dashboard from './pages/Dashboard';
+import OptionsChain from './pages/OptionsChain';
+import Portfolio from './pages/Portfolio';
+import StrategyBuilder from './pages/StrategyBuilder';
+import Watchlist from './pages/Watchlist';
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-      <p>Start prompting (or editing) to see magic happen :)</p>
-    </div>
+    <Router>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/options-chain" element={<OptionsChain />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+          <Route path="/strategy-builder" element={<StrategyBuilder />} />
+          <Route path="/watchlist" element={<Watchlist />} />
+        </Route>
+      </Routes>
+    </Router>
   );
 }
 
