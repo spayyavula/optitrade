2124c56b1914729dc63139e11b42a9c22fa39374
Sreekanth <spayyavula@gmail.com>
2025-06-05 15:25:26 -0700

    Deploy application using echo

---
+++ echo.config.js
@@ -0,0 +1,18 @@
+export default {
+  name: 'options-trading-platform',
+  output: 'dist',
+  static: true,
+  routes: [
+    '/',
+    '/options-chain',
+    '/portfolio',
+    '/strategy-builder',
+    '/watchlist',
+    '/scanner',
+    '/learn'
+  ],
+  build: {
+    command: 'npm run build',
+    directory: 'dist'
+  }
+}
